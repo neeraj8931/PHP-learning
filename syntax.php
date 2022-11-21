12000c0f300bbdd86a61d9850562aa385c27b364
Neeraj Shukla <er.neerajshukla.1010@gmail.com>
2022-11-22 00:07:39 +0530

    php syntex

---
+++ syntax.php
@@ -0,0 +1,13 @@
+<?php  
+$carName = "Maruti";
+// variables names are case sensetive in php while php keywords are not sensitive
+
+echo "My car name is $carName";
+eCho "My car name is $carName";
+ECHO "My car name is $carName";
+
+echo "My car name is $carNAme"; //PHP Warning:  Undefined variable $carNAme 
+echo "My car name is $CARNAME";  //PHP Warning:  Undefined variable $CARNAME 
+
+
+?>
